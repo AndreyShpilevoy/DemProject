806762084456487e64741f125e709cb35201b324
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-01-27 01:03:07 +0200

    add some scenarios to gulp (concat without minimization) add dem.min.js to _Layout.cshtml rewrite to ts getToPosition.js, getUrlParametr.js and WorkWithCoocies.js add 2 simple collection to ts (dictionary and list)

---
+++ DEM_MVC/wwwroot/TypeScripts/GetUrlParametr.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../TypingsForTypeScript/tsd.d.ts" />
+
+class UrlParametrService {
+    pageUrl: string;
+    urlVariablesArray: string[];
+    searchingParamName: string;
+    nameValueParamArray: string[];
+
+    constructor() {
+        this.pageUrl = decodeURIComponent(window.location.search.substring(1));
+        this.urlVariablesArray = this.pageUrl.split("&");
+    }
+
+    getParametrByName(parameterName: string) :number {
+        this.searchingParamName = parameterName;
+
+        for (var i = 0; i < this.urlVariablesArray.length; i++) {
+            this.nameValueParamArray = this.urlVariablesArray[i].split("=");
+
+            if (this.nameValueParamArray[0] === this.searchingParamName) {
+                return this.nameValueParamArray[1] === undefined ? 0 : parseInt(this.nameValueParamArray[1]);
+            }
+        }
+    }
+}
---
+++ DEM_MVC/wwwroot/TypeScripts/ScrollPageService.ts
@@ -0,0 +1,59 @@
+﻿/// <reference path="../TypingsForTypeScript/tsd.d.ts" />
+
+class ScrollPageService {
+    isVisible: boolean;
+    isTower: boolean;
+
+    onScroll() {
+        if ($(document).scrollTop() > 150) {
+            if (this.isVisible) return;
+            this.isVisible = true;
+            $("#back-to-top").stop(true, true).fadeIn();
+        }
+        else {
+            if (!this.isVisible) return;
+            this.isVisible = false;
+            $("#back-to-top").stop(true, true).fadeOut();
+        }
+    }
+
+    onResize() {
+        if ($(document).width() - $("#wrap").width() > 120) {
+            if (this.isTower) return;
+            this.isTower = true;
+            $("#back-to-top").addClass("tower");
+        }
+        else {
+            if (!this.isTower) return;
+            this.isTower = false;
+            $("#back-to-top").removeClass("tower");
+        }
+    }
+
+    goToTop() {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    }
+
+    goToPost(postId: number) {
+        $("html, body").animate({ scrollTop: $(`#postId${postId}`).offset().top }, 500);
+    }
+}
+
+
+var scrollPageToTopService = new ScrollPageService();
+
+window.onload = () => {
+    if ($("#back-to-top").length) {
+        $(window).scroll(scrollPageToTopService.onScroll);
+        $(window).resize(scrollPageToTopService.onResize);
+        $("#back-to-top").click(() => {
+            scrollPageToTopService.goToTop();
+        });
+    }
+
+    var urlParametrService = new UrlParametrService();
+    var postId = urlParametrService.getParametrByName("postId");
+    if (postId) {
+        scrollPageToTopService.goToPost(postId);
+    }
+};
