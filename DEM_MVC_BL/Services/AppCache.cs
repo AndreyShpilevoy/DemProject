af8bc62d2aaed6e3f1253a8cb4c9b8e02109c9b8
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-02-28 21:13:46 +0200

    fix Caching for bbCodes

--- DEM_MVC_BL/Services/AppCache.cs
+++ DEM_MVC_BL/Services/AppCache.cs
@@ -13,7 +13,18 @@ namespace DEM_MVC_BL.Services
             return memoryCache.Get(key) as List<T>;
         }
 
+        public Dictionary<T, TT> Get<T, TT>(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Get(key) as Dictionary<T, TT>;
+        }
+
         public bool Add<T>(List<T> value, string key)
+        {
+            return Add(value, key, 20);
+        }
+
+        public bool Add<T, TT>(Dictionary<T, TT> value, string key)
         {
             return Add(value, key, 20);
         }
@@ -24,12 +35,29 @@ namespace DEM_MVC_BL.Services
             return memoryCache.Add(key, value, DateTime.Now.AddMinutes(expirationInMinutes));
         }
 
+        public bool Add<T, TT>(Dictionary<T, TT> value, string key, int expirationInMinutes)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Add(key, value, DateTime.Now.AddMinutes(expirationInMinutes));
+        }
+
         public void Update<T>(List<T> value, string key)
         {
             Update(value, key, 20);
         }
 
+        public void Update<T, TT>(Dictionary<T, TT> value, string key)
+        {
+            Update(value, key, 20);
+        }
+
         public void Update<T>(List<T> value, string key, int expirationInMinutes)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            memoryCache.Set(key, value, DateTime.Now.AddMinutes(expirationInMinutes));
+        }
+
+        public void Update<T, TT>(Dictionary<T, TT> value, string key, int expirationInMinutes)
         {
             MemoryCache memoryCache = MemoryCache.Default;
             memoryCache.Set(key, value, DateTime.Now.AddMinutes(expirationInMinutes));
