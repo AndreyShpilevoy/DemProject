41679e7e762f3444a9a8733511c2fe882e4f5068
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-07-21 00:44:42 +0300

    missed from previous commit

--- DEM_MVC/wwwroot/src/scripts/components/forums/ForumsPage.js
+++ DEM_MVC/wwwroot/src/scripts/components/forums/ForumsPage.js
@@ -1,64 +1,62 @@
-import React, {PropTypes}  from "react";
-import { connect } from "react-redux";
-import * as forumAction from "../../actions/forumActions";
+import React, {PropTypes} from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import * as forumActions from "../../actions/forumActions";
 
-class ForumsPage extends React.Component{
+class ForumsPage extends React.Component {
 
-  constructor(props, context){
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            forum: {
+                title: ""
+            }
+        };
 
-    this.state = {
-      forum: {
-        title: ""
-      }
-    };
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
 
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
+    onTitleChange(event) {
+        const forum = this.state.forum;
+        forum.title = event.target.value;
+        this.setState({forum: forum});
+    }
 
-  onTitleChange(event){
-    const forum = this.state.forum;
-    forum.title = event.target.value;
-    this.setState({forum: forum});
-  }
+    onClickSave() {
+        this.props.actions.createForum(this.state.forum);
+    }
 
-  onClickSave(){
-    this.props.dispatch(forumAction.createForum(this.state.forum));
-  }
+    forumRow(forum, index) {
+        return <div key={index}>{forum.title}</div>;
+    }
 
-  forumRow(forum, index){
-    return <div key={index}>{forum.title}</div>;
-  }
-
-  render(){
-    return(
-      <div>
-        <h2>Test ForumsPage</h2>
-        {this.props.forums.map(this.forumRow)}
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.forum.title}/>
-        <input
-          type="submit"
-          onClick={this.onClickSave}
-          value="Save"/>
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state, ownProps){
-  return {
-    forums: state.forumReducer
-  };
+    render() {
+        return (
+            <div>
+                <h2>Test ForumsPage</h2>
+                {this.props.forums.map(this.forumRow)}
+                <input type="text" onChange={this.onTitleChange} value={this.state.forum.title}/>
+                <input type="submit" onClick={this.onClickSave} value="Save"/>
+            </div>
+        );
+    }
 }
 
 ForumsPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  forums: PropTypes.array.isRequired
+    forums: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
-const connectedStateAndProps = connect(mapStateToProps);
+function mapStateToProps(state, ownProps) {
+    return {forums: state.forumReducer};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(forumActions, dispatch)
+    };
+}
+
+const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
 export default connectedStateAndProps(ForumsPage);
