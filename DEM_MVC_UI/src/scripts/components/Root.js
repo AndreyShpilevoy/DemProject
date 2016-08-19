8a55d4da62df51242104804aaaf3d68f5745cdc5
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-08-19 12:30:14 +0300

    turn on added react-router-reduser deleted unused lib's

--- DEM_MVC_UI/src/scripts/components/Root.js
+++ DEM_MVC_UI/src/scripts/components/Root.js
@@ -1,16 +1,26 @@
 import React, {PropTypes} from "react";
 import {Provider} from "react-redux";
 import {Router, browserHistory} from "react-router";
+import { syncHistoryWithStore } from 'react-router-redux';
 
-const Root = ({store, routes}) => (
-  <Provider store={store}>
-    <Router history={browserHistory} routes={routes}/>
-  </Provider>
-);
+class Root  extends React.Component{
+  static propTypes = {
+    store: PropTypes.object.isRequired,
+    routes: PropTypes.object.isRequired
+  };
 
-Root.propTypes = {
-  store: PropTypes.object.isRequired,
-  routes: PropTypes.object.isRequired
-};
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    const history = syncHistoryWithStore(browserHistory, this.props.store);
+    return (
+      <Provider store={this.props.store}>
+        <Router history={history} routes={this.props.routes}/>
+      </Provider>
+    );
+  }
+}
 
 export default Root;
--- DEM_MVC_UI/src/scripts/reducers/rootReducer.js
+++ DEM_MVC_UI/src/scripts/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
+import { routerReducer } from 'react-router-redux';
 import forumReducer from "./forumReducer";
 import authorReducer from "./authorReducer";
 
 const rootReducer = combineReducers({
   forumReducer,
-  authorReducer
+  authorReducer,
+  routing: routerReducer
 });
 
 export default rootReducer;
