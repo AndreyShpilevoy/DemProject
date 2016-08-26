8a07063e2545cb8007d106fc75e4d32626723e90
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-08-26 18:35:38 +0300

    changed first example test to another component

--- DEM_MVC_UI/__tests__/components/common/Header-Test.js
+++
@@ -1,9 +0,0 @@
-/*eslint no-undef: "off"*/
-
-describe("the header module", () => {
-  describe("the header navigation container", () => {
-    it("should create <nav> tag", () => {
-
-    });
-  });
-});
---
+++ DEM_MVC_UI/__tests__/components/topics/TopicsPage-test.js
@@ -0,0 +1,9 @@
+/*eslint no-undef: "off"*/
+
+describe("the TopicsPage module", () => {
+  describe("the TopicsPage container", () => {
+    it("should have 'Test TopicsPage' in <p> tag", () => {
+
+    });
+  });
+});
