cf531ff63cac04acdb2fa353a102f85f2a3ff6fa
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-08-26 17:49:46 +0300

    added one simple test without any checks

---
+++ DEM_MVC_UI/__tests__/components/common/Header-Test.js
@@ -0,0 +1,9 @@
+/*eslint no-undef: "off"*/
+
+describe("the header module", () => {
+  describe("the header navigation container", () => {
+    it("should create <nav> tag", () => {
+
+    });
+  });
+});
