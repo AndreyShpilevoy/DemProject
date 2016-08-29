a26deb2fe568c8f55c875d2de84e6ec5b9a0d5de
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-08-29 17:47:57 +0300

    start implement Header html and css structure

--- DEM_MVC_UI/src/scripts/components/common/presentation/Header.js
+++ DEM_MVC_UI/src/scripts/components/common/presentation/Header.js
@@ -6,13 +6,19 @@ const Header = () => {
     <div className="container navbar-fixed-top">
       <div className="row">
         <div className="col-xs-12">
-          <nav>
-            <IndexLink to="/" activeClassName="active">ForumsPage</IndexLink>
-            {" | "}
-            <Link to="/topics" activeClassName="active">TopicsPage</Link>
+          <nav id="header" className="navbar navbar-full navbar-height navbar-logo-bg">
+            <div className="col-xs-12 col-lg-6">
+              //logo
+            </div>
+            <div className="col-xs-12 col-lg-6 padding-initial-xs-up">
+              //links
+            </div>
           </nav>
         </div>
       </div>
+        <IndexLink to="/" activeClassName="active">ForumsPage</IndexLink>
+        {" | "}
+        <Link to="/topics" activeClassName="active">TopicsPage</Link>
     </div>
   );
 };
