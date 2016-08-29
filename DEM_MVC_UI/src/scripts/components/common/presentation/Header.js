e24d15386490c9c394ecd1e4d8a10f0f5349194f
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-08-29 23:58:34 +0300

    added one more bootstrap module change size of header and footer added logo to header added partially tougle button for menu added two scss classes to clear margin and padding

--- DEM_MVC_UI/src/scripts/components/common/presentation/Header.js
+++ DEM_MVC_UI/src/scripts/components/common/presentation/Header.js
@@ -4,23 +4,34 @@ import { Link, IndexLink } from 'react-router';
 const Header = () => {
   return(
     <div className="container navbar-fixed-top">
-      <div className="row">
-        <div className="col-xs-12">
-          <nav id="header" className="navbar navbar-full navbar-height navbar-logo-bg">
-            <div className="col-xs-12 col-lg-6">
-              //logo
-            </div>
-            <div className="col-xs-12 col-lg-6 padding-initial-xs-up">
-              //links
+      <div className="row margin-initial">
+        <div className="col-xs-12 padding-initial">
+          <nav id="header" className="navbar navbar-full navbar-height navbar-logo-bg padding-initial">
+            <div className="row margin-initial">
+              <div className="col-xs-12 col-lg-6 padding-initial">
+                <a className="navbar-brand navbar-brand-size" href="#">
+                  <div className="navbar-logo"/>
+                </a>
+                // <button type="button" className="navbar-toggler hidden-lg-up pull-xs-right" id="menu-toggle-button">
+								// 	<span className="sr-only">Toggle navigation</span>
+								// 	<span className="icon-bar"/>
+								// 	<span className="icon-bar"/>
+								// 	<span className="icon-bar"/>
+								// </button>
+              </div>
+              <div className="col-xs-12 col-lg-6 padding-initial">
+                //links
+              </div>
             </div>
           </nav>
         </div>
       </div>
-        <IndexLink to="/" activeClassName="active">ForumsPage</IndexLink>
-        {" | "}
-        <Link to="/topics" activeClassName="active">TopicsPage</Link>
     </div>
   );
 };
+
+// <IndexLink to="/" activeClassName="active">ForumsPage</IndexLink>
+// {" | "}
+// <Link to="/topics" activeClassName="active">TopicsPage</Link>
 
 export default Header;
