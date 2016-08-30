b46962c8578fc04ca0351f253e4fed1bd1edc1da
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-08-30 11:25:01 +0300

    added one more bootstrap scss dependency added and styled button for toggle munu on small screens

--- DEM_MVC_UI/src/scripts/components/common/presentation/Header.js
+++ DEM_MVC_UI/src/scripts/components/common/presentation/Header.js
@@ -7,20 +7,24 @@ const Header = () => {
       <div className="row margin-initial">
         <div className="col-xs-12 padding-initial">
           <nav id="header" className="navbar navbar-full navbar-height navbar-logo-bg padding-initial">
-            <div className="row margin-initial">
-              <div className="col-xs-12 col-lg-6 padding-initial">
-                <a className="navbar-brand navbar-brand-size" href="#">
-                  <div className="navbar-logo"/>
-                </a>
-                // <button type="button" className="navbar-toggler hidden-lg-up pull-xs-right" id="menu-toggle-button">
-								// 	<span className="sr-only">Toggle navigation</span>
-								// 	<span className="icon-bar"/>
-								// 	<span className="icon-bar"/>
-								// 	<span className="icon-bar"/>
-								// </button>
+            <div className="row margin-initial heigth-inherit">
+              <div className="flex-container col-xs-12 col-lg-6 padding-initial heigth-inherit">
+                <div className="flex-element-1 flex-container-left flex-container-top-center">
+                  <a className="navbar-brand navbar-brand-size" href="/">
+                    <div className="navbar-logo"/>
+                  </a>
+                </div>
+                <div className="flex-element-1 flex-container-right flex-container-top-center">
+                  <button type="button" className="hidden-lg-up navbar-toggler" id="menu-toggle-button">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"/>
+                    <span className="icon-bar"/>
+                    <span className="icon-bar"/>
+                  </button>
+                </div>
               </div>
               <div className="col-xs-12 col-lg-6 padding-initial">
-                //links
+
               </div>
             </div>
           </nav>
