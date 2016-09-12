023b1a19f9a46606dc342bb055c70975b5dc49a8
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-09-12 23:09:31 +0300

    change structure of scss files change navigation link css selector to navigation link item

--- DEM_MVC_UI/src/scripts/components/NavigationLinkItem.js
+++ DEM_MVC_UI/src/scripts/components/NavigationLinkItem.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 const NavigationLinkItem = ({navigationLinkItem}) => {
   return (
-    <li className="nav-item">
+    <li className="nav-list-item">
       <Link className="nav-link" to={navigationLinkItem.href}>{navigationLinkItem.title}</Link>
       <div className="nav-link-separator"/>
     </li>
