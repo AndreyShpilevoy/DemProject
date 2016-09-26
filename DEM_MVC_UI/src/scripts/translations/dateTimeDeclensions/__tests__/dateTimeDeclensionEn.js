ea271b34e118f672984904346173344ee9fcaf1b
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-09-26 17:32:49 +0300

    Added tests for dateTimeDeclension's functionality

---
+++ DEM_MVC_UI/src/scripts/translations/dateTimeDeclensions/__tests__/dateTimeDeclensionEn.js
@@ -0,0 +1,17 @@
+/*eslint no-undef: "off"*/
+
+import dateTimeDeclensionEn from "../dateTimeDeclensionEn";
+
+describe('dateTimeDeclensionEn', () => {
+
+  it('should return "singleForm"',() => {
+    let result = dateTimeDeclensionEn(1,"firstForm","secondForm","trirdForm","singleForm");
+    expect(result).toBe("singleForm");
+  });
+
+  it('should return "2 secondForm"',() => {
+    let result = dateTimeDeclensionEn(2,"firstForm","secondForm","trirdForm","singleForm");
+    expect(result).toBe("2 secondForm");
+  });
+
+});
