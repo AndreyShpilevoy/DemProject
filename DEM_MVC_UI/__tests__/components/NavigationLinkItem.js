dfdfafa1d34b85411df19ac51cd99a1e3dc175e7
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-09-26 14:26:39 +0300

    updated test

--- DEM_MVC_UI/__tests__/components/NavigationLinkItem.js
+++ DEM_MVC_UI/__tests__/components/NavigationLinkItem.js
@@ -15,25 +15,26 @@ describe('NavigationLinkItem', () => {
 
   it('should render top level li',() => {
     const liElement = setup().find('li');
-    expect(liElement.length).toBe(1);
     expect(liElement.hasClass("nav-list-item")).toBeTruthy();
   });
 
   it('should render Link component',() => {
     const linkComponent = setup().find('Link');
-    expect(linkComponent.length).toBe(1);
     expect(linkComponent.hasClass("nav-link")).toBeTruthy();
   });
 
   it('Link component has title equel to "testNavigationLink"',() => {
     const linkComponent = setup().find('Link');
-    expect(linkComponent.length).toBe(1);
     expect(linkComponent.prop("children")).toBe('testNavigationLink');
+  });
+
+  it('Link component has prop "to" equel to "http://testNavigationHref"',() => {
+    const linkComponent = setup().find('Link');
+    expect(linkComponent.prop("to")).toBe("http://testNavigationHref");
   });
 
   it('should render div for Navigation Link Separator',() => {
     const divElement = setup().find('div');
-    expect(divElement.length).toBe(1);
     expect(divElement.hasClass("nav-link-separator")).toBeTruthy();
   });
 
