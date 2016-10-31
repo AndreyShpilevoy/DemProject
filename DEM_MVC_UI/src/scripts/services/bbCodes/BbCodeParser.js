e4a832dfaf0b9a1e89e54e3ac55c6fbf5d04050a
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-10-31 15:06:49 +0200

    adjusted bbCodeParser - now it parce all bbCodes and ignore all newLines

--- DEM_MVC_UI/src/scripts/services/bbCodes/BbCodeParser.js
+++ DEM_MVC_UI/src/scripts/services/bbCodes/BbCodeParser.js
@@ -3,8 +3,7 @@ import * as bbCodeTypes from "enums/bbCodeTypes";
 class BbCodeParser{
   getParsedTree = (text) => {
     let allTags = this.getAllTags(text);
-    debugger;
-    //this.buildTree(allTags);
+    this.buildTree(allTags);
   }
 
   getAllTags = (text) => {
@@ -21,7 +20,7 @@ class BbCodeParser{
       let matchResult = {
         type: match[1] ? bbCodeTypes.NEW_LINE : match[2] ? bbCodeTypes.OPEN_TAG : bbCodeTypes.CLOSE_TAG,
         match: match[0],
-        tag: match[2],
+        tag: match[2] ? match[2] : match[4],
         options: match[3],
         firstIndex: match.index,
         lastIndex: regex.lastIndex,
@@ -32,13 +31,49 @@ class BbCodeParser{
     return result;
   }
 
-  // buildTree = (tagsArray) => {
-  //   for(let tag of tagsArray){
-  //
-  //   }
-  // }
+  buildTree = (tagsArray) => {
+    let nodeTree = [];
+    for(let index = 0; index < tagsArray.length;){
+      let resultObject = this.getNode(tagsArray[index], index+1, tagsArray);
+      index = resultObject.index;
+      if(resultObject.node) {
+        nodeTree.push(resultObject.node);
+      }
+    }
+  }
 
-  //getNode =
+  getNode = (tag, index, tagsArray) => {
+    let node = {
+      openTag: tag,
+      closeTag: null,
+      children: []
+    };
+
+    let i = index;
+    for(; i < tagsArray.length;){
+      if(tagsArray[i].type === bbCodeTypes.CLOSE_TAG &&
+        tagsArray[i].tag === tag.tag){
+        node.closeTag = tagsArray[i];
+        i++;
+        break;
+      }
+      else if(tagsArray[i].type === bbCodeTypes.OPEN_TAG){
+        let childNodeResultObject = this.getNode(tagsArray[i], i+1,tagsArray);
+        if(childNodeResultObject){
+          node.children.push(childNodeResultObject.node);
+          i = childNodeResultObject.index;
+        }
+      }
+      else{
+        i++;
+      }
+    }
+    if(node.closeTag) {
+      return {node, index:i};
+    } else {
+      return {node:null, index: index+1};
+    }
+  }
 }
 
 export default new BbCodeParser();
